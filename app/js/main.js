42c582875fba5070f739efde1cf2c07af252b5f8
sethmcleod <me@sethmcleod.com>
2015-08-25 15:45:03 -0400

    NoJS Fallback

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+document.getElementById('body-overlay').style['z-index'] = 999;
 document.getElementById('body-overlay').onclick = function() {
   document.getElementById('nav-toggle').checked = false;
 };
