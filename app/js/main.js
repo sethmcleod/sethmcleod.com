64d4080fd362c6069941dceca8221167f0a917f2
sethmcleod <me@sethmcleod.com>
2015-11-17 10:31:19 -0500

    IIFE Wrapper

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,22 @@
-document.getElementById('body-overlay').style['z-index'] = 999;
-document.getElementById('body-overlay').onclick = function() {
-  document.getElementById('nav-toggle').checked = false;
-};
+(function() {
+
+  // clickable wrapper
+  document.getElementById('wrapper').onclick = function() {
+    document.getElementById('nav-toggle').checked = false;
+  };
+
+  // sticky header nav
+  // var banner = document.getElementById('banner'),
+  //     header = document.getElementById('header'),
+  //     bottom = banner.getBoundingClientRect().bottom;
+  //
+  // window.addEventListener("scroll", function(event) {
+  //   if (window.pageYOffset > bottom) {
+  //     header.classList.add('fixed');
+  //   }
+  //   if (window.pageYOffset <= bottom) {
+  //     header.classList.remove('fixed');
+  //   }
+  // });
+
+})();
