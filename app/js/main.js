3113d31607c791f451674eb4c7d5fc484ce5777e
sethmcleod <me@sethmcleod.com>
2016-01-15 06:00:52 -0500

    Sticky Header

--- app/js/main.js
+++ app/js/main.js
@@ -6,17 +6,17 @@
   };
 
   // sticky header nav
-  // var banner = document.getElementById('banner'),
-  //     header = document.getElementById('header'),
-  //     bottom = banner.getBoundingClientRect().bottom;
-  //
-  // window.addEventListener("scroll", function(event) {
-  //   if (window.pageYOffset > bottom) {
-  //     header.classList.add('fixed');
-  //   }
-  //   if (window.pageYOffset <= bottom) {
-  //     header.classList.remove('fixed');
-  //   }
-  // });
+  var banner = document.getElementById('banner'),
+      header = document.getElementById('header'),
+      bottom = banner.getBoundingClientRect().bottom;
+
+  window.addEventListener("scroll", function(event) {
+    if (window.pageYOffset > bottom) {
+      header.classList.add('fixed');
+    }
+    if (window.pageYOffset === 0 || window.pageYOffset <= bottom) {
+      header.classList.remove('fixed');
+    }
+  });
 
 })();
