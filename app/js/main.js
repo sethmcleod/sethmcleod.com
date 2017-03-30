06983b77404d9b29c025127d4a6cf45cf263cd9b
sethmcleod <me@sethmcleod.com>
2017-03-30 10:44:16 -0400

    Purge unneeded code

--- app/js/main.js
+++ app/js/main.js
@@ -3,26 +3,7 @@
   // remove no-fouc class
   document.documentElement.removeAttribute('class');
 
-  // clickable wrapper
-  document.getElementById('wrapper').onclick = function() {
-    document.getElementById('nav-toggle').checked = false;
-  };
-
-  // sticky header nav
-  var banner = document.getElementById('banner'),
-      header = document.getElementById('header'),
-      email = document.getElementById('email'),
-      bottom = banner.getBoundingClientRect().bottom;
-
-  window.addEventListener("scroll", function(event) {
-    if (window.pageYOffset > bottom) {
-      header.classList.add('fixed');
-    }
-    if (window.pageYOffset === 0 || window.pageYOffset <= bottom) {
-      header.classList.remove('fixed');
-    }
-  });
-
+  // copy email to clipboard
   email.addEventListener('click', function(event) {
     copyTextToClipboard('me@sethmcleod.com');
   });
