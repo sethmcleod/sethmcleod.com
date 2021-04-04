a5489f51540c0e67682d049ea26d3d59fe01c1e3
Seth Mcleod <me@sethmcleod.com>
2021-04-04 14:53:58 -0700

    Wire up custom config and external font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  purge: [
-    './src/**/*.html'
-  ],
+  purge: ['./src/**/*.html'],
   darkMode: 'media', // 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ['Proza Libre', ...defaultTheme.fontFamily.sans],
+      serif: ['Quando', ...defaultTheme.fontFamily.serif],
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
